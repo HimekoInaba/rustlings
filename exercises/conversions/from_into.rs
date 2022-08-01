cc62593d1ff52f52e6f623246a34105516a09970
Syrym Tussupbekov <justsyrym@gmail.com>
2022-08-01 22:43:48 +0600

    finish all exercises

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,31 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let split: Vec<String> = s.split(",")
+            .map(|x| String::from(x))
+            .collect();
+        if split.len() != 2 {
+            return Person::default();
+        }
+        let name = split.get(0);
+        return if let Some(x) = name {
+            let name = x.clone();
+            if name.len() == 0 {
+                return Person::default();
+            }
+            let age = split.get(1).unwrap().parse::<usize>();
+            match age {
+                Ok(age) => Person { name, age },
+                Err(_ParseIntError) => Person::default()
+            }
+        } else {
+            Person::default()
+        }
     }
 }
 
